37fdc46df45149d820c33ab6d2727c4494cf2d4a
famaridon <famaridon@users.noreply.github.com>
2019-09-03 13:17:47 +0200

    boot

---
+++ src/main/java/com/code/some/games/tegback/TegBackApplication.java
@@ -0,0 +1,13 @@
+package com.code.some.games.tegback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TegBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TegBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/some/games/tegback/TegBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.code.some.games.tegback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TegBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
