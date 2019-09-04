4efbb496264d1fce3c80167faf05e012132576a2
famaridon <famaridon@users.noreply.github.com>
2019-09-04 13:43:19 +0200

    setup web scoket

--- src/main/java/com/code/some/games/tegback/rest/v1/WSRoomController.java
+++ src/main/java/com/code/some/games/tegback/rest/v1/WSRoomController.java
@@ -2,17 +2,19 @@ package com.code.some.games.tegback.rest.v1;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path = "teg")
-public class TegGameController {
+@Controller
+public class WSRoomController {
 
-  @GetMapping(path = "ping")
-  public String ping(){
-    return "{\"result\":\"pong\"}";
+  @MessageMapping("/events/play")
+  @SendTo("/events/update")
+  public String play(String message) throws Exception {
+    return "ping";
   }
 
 }
